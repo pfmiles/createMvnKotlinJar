e0f66fd13b2188d4dc4ba3318b919bc596e9185c
pf_miles <miles.wy.1@gmail.com>
2024-02-26 11:10:32 +0800

    init commit

---
+++ src/main/kotlin/com/github/pfmiles/createmvnkotlinjar/CreateMvnKtJar.kt
@@ -0,0 +1,10 @@
+package com.github.pfmiles.createmvnkotlinjar
+
+class CreateMvnKtJar {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("hello")
+        }
+    }
+}
